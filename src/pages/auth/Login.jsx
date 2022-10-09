e9858fd4ed9426e34c8a8b6372ecc90eaabd5c1a
Egor-oop <gulidoegor@gmail.com>
2022-10-09 14:19:05 +0300

    feat: redesigned login page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Auth.css';
-import { AuthInput } from '../../components/auth/AuthInput';
 import { InputButton } from '../../components/button/InputButton';
+import { MainButton } from '../../components/button/MainButton';
 import { MainInput } from '../../components/input/MainInput';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 
@@ -17,16 +17,16 @@ export const Login = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({username: username, password: password}),
+      body: JSON.stringify({ username: username, password: password }),
     })
-    .then((response) => response.json())
-    .then((data) => {
-      localStorage.setItem('userToken', data.token);
-      navigate('/profile')
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem('userToken', data.token);
+        navigate('/profile')
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
 
   const isLoggedIn = !localStorage.getItem('userToken');
@@ -36,39 +36,33 @@ export const Login = () => {
         <form className='auth__form'>
           <h1 className='auth__header'>Log In</h1>
           <div className='auth__form-inputs'>
-            <MainInput
-              type='text'
-              placeholder='Username'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              style={{width:'425px'}}
-            />
-            <MainInput
-              type='password'
-              placeholder='Password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              style={{width:'425px'}}
-            />
-            {/* <AuthInput
-              type='text'
-              label='Username'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <AuthInput
-              type='password'
-              label='Password'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            /> */}
-            <InputButton
+            <div className='auth__form-data-inputs'>
+              <MainInput
+                type='text'
+                placeholder='Username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                style={{ width: '425px' }}
+                required
+              />
+              <MainInput
+                type='password'
+                placeholder='Password'
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ width: '425px' }}
+                required
+              />
+            </div>
+            <MainButton
               title='Log In'
               onClick={() => login()}
+              style={{'max-width': '120px'}}
+              wrapperStyle={{'text-align': 'center'}}
             />
           </div>
-          <p>
-            Don't have an account? <Link to='/register'>Register</Link>
+          <p className='auth__question-text'>
+            Don't have an account? <Link to='/register'>Register new</Link>
           </p>
         </form>
       </div>
